61f83c5edaf90163f2a1e533513e555df913cec8
Braeden Smith <braedensmith2@gmail.com>
2015-12-04 21:10:18 -0500

    Create aliases.sh

---
+++ Chromebook/aliases.sh
@@ -0,0 +1,9 @@
+alias ll='ls -alF'
+alias grades='/home/braeden/grades.sh'
+alias la='ls -A'
+alias l='ls -CF'
+alias gitpush='git add -A && git commit -m "commit $(date +%Y %m %d)" && git push -u origin master'
+alias gitpull='git pull origin master'
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
