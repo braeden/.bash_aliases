02bbd7699701d5c467502e16a1f0ab35a54c0054
Braeden Smith <braedensmith2@gmail.com>
2016-02-01 09:16:53 -0500

    Add apache and git

--- Chromebook/aliases.sh
+++ Chromebook/aliases.sh
@@ -5,6 +5,9 @@ alias la='ls -A'
 alias l='ls -CF'
 alias gitpush='git add -A && git commit -m "commit $(date +%Y %m %d)" && git push -u origin master'
 alias gitpull='git pull origin master'
+alias working='cd ~/sketchbook/Projects/orbits'
+alias start_apache='sudo /etc/init.d/apache2 start'
+alias restart_apache='sudo /etc/init.d/apache2 restart'
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
