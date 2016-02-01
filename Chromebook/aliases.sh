4e46a655d67a6069950f50eb6221384e818f81f9
Braeden Smith <braedensmith2@gmail.com>
2016-02-01 09:19:27 -0500

    Add cd to apache dir

--- Chromebook/aliases.sh
+++ Chromebook/aliases.sh
@@ -6,8 +6,9 @@ alias l='ls -CF'
 alias gitpush='git add -A && git commit -m "commit $(date +%Y %m %d)" && git push -u origin master'
 alias gitpull='git pull origin master'
 alias working='cd ~/sketchbook/Projects/orbits'
-alias start_apache='sudo /etc/init.d/apache2 start'
-alias restart_apache='sudo /etc/init.d/apache2 restart'
+alias start_apache='sudo /etc/init.d/apache2 start; cd /var/www/'
+alias restart_apache='sudo /etc/init.d/apache2 restart; cd /var/www/'
+
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
